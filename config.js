41af7f3e502006f2db15c920b5be1cff56007bcd
TabishRizvi <sayyidtabish@gmail.com>
2016-10-13 23:08:53 +0530

    test 7

---
+++ config.js
@@ -0,0 +1,21 @@
+/**
+ * Created by Tabish Rizvi.
+ * Github :  https://github.com/TabishRizvi
+ * Email : sayyidtabish@gmail.com
+ */
+
+"use strict";
+
+module.exports.contentType = {
+
+    json : "application/json",
+    urlencoded : "application/x-www-form-urlencoded",
+    multipart : "multipart/form-data"
+};
+
+module.exports.validSchemes =  [
+    "http",
+    "https"
+];
+
+
